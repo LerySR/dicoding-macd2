11fc1acc9f5905e10ff21c0061d00615d9d0bc49
fadhilelrizanda <59647673+fadhilelrizanda@users.noreply.github.com>
2020-01-22 23:39:19 +0700

    Update phpQS.php

--- phpQS.php
+++ phpQS.php
@@ -1,7 +1,7 @@
 
 <?php
 
-$target_file = '/'($_FILES["fileToUpload"]["name"]["type"]);
+$target_file = $_FILES["fileToUpload"]["name"]["type"];
 /**----------------------------------------------------------------------------------
 * Microsoft Developer & Platform Evangelism
 *
