5f3729e2fe063f3135d50716a718579f3f180f6d
fadhilelrizanda <59647673+fadhilelrizanda@users.noreply.github.com>
2020-01-22 21:00:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,12 @@
 <body>
 <h1>Masukkan file untuk diunggah ke blob azure</h1>
 <form action="phpQs.php" method="post" enctype="multipart/form-data">
+    
     Select file to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload file" name="submit">
-</form>
+    
+    </form>
 <script type="text/javascript">
     function processImage() {
         // **********************************************
